e5f35f3da6b4cd3be6de91fee9c88ef5a2a401a5
ryzech <jake.a.coppinger@gmail.com>
2021-11-07 20:07:43 -0600

    last commit again

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={`Home`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
